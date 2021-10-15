3bbf6780f27b1f33e17dfc82135ab2e09bc07a37
Tsutomu Ikeda <40418321+Tsutomu-Ikeda@users.noreply.github.com>
2021-10-15 10:42:49 +0900

    urlのマッチ条件を修正し、Block Kit Builderでタブが閉じることがないようにした

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (
     changeInfo.status === "complete" &&
-    tab.url.match(/^https:\/\/(.+\.)?(slack\.com\/archives\/.+|zoom\.us\/j\/)/)
+    tab.url.match(/^https:\/\/([^/]+\.)?(slack\.com\/archives\/.+|zoom\.us\/j\/)/)
   ) {
     setTimeout(() => {
       chrome.tabs.remove(tabId);
